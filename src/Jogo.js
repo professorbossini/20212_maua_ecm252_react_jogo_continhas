126b5c89e0da33eb58850f243b2b593a13fc09f3
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-11 12:52:08 -0300

    Fizemos ate o bloco de codigo 2.7.4 da apostila 06

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,50 +1,170 @@
 import React, { Component } from 'react'
-import { Card } from 'primereact/card'
+import _ from 'underscore'
 import { Button } from 'primereact/button'
+
 export default class Jogo extends Component {
-    render() {
+    constructor (props){
+        super(props)
+        this.state = {
+            alternativas: Array(5).fill(undefined),
+            intervaloDeAtualizacao: 5000,
+            tempoRestante: 5
+
+        }
+    }
+    
+    valorInicial = 1
+    valorFinal = 10
+
+    timerGeral = null
+    timerSegundoASegundo = null
+
+    operacoes = [
+        {
+            simbolo: '+', operacao: (a, b) => a + b,
+        },
+        {
+            simbolo: '-', operacao: (a, b) => a - b
+        }
+    ]
+
+    gerarConta = () => {
+        let n1 = Math.floor(Math.random() * this.valorFinal) + this.valorInicial
+        let n2 = Math.floor(Math.random() * this.valorFinal) + this.valorInicial
+        let oQueFazer = Math.floor(Math.random() * this.operacoes.length)
+        let simbolo = this.operacoes[oQueFazer]['simbolo']
+        let resultado = this.operacoes[oQueFazer]['operacao'](n1, n2)
+        return {n1, n2, simbolo, resultado}
+    }
+
+    gerarAlternativas = (resultado) => {
+        let aux = [resultado]
+        while (aux.length < 5){
+            let n = Math.floor (Math.random() * this.valorFinal) + this.valorInicial
+            if (!aux.includes(n))
+                aux.push(n)
+        }
+        return _.shuffle(aux)
+    }
+
+    gerarJogo = () => {
+        let {n1, n2, simbolo, resultado} = this.gerarConta()
+        let alternativas = this.gerarAlternativas(resultado)
+        this.setState({
+            n1, n2, resultado, simbolo, alternativas, tempoRestante: 5
+        })
+    }
+
+    encerrar = () => {
+        clearInterval(this.timerGeral)
+        clearInterval(this.timerSegundoASegundo)
+    }
+
+    componentDidMount(){
+        this.iniciarRodada()
+    }
+
+    componentWillUnmount(){
+        this.encerrar()
+    }
+
+    iniciarRodada = () => {
+        clearInterval(this.timerGeral)
+        clearInterval(this.timerSegundoASegundo)
+        let fire = (f, t) => {
+            f()
+            this.timerSegundoASegundo = setInterval (() => {
+                this.setState({tempoRestante: this.state.tempoRestante - 1})
+            }, 1000)
+            return setInterval(f, t)
+        }
+        this.timerGeral = fire(this.gerarJogo, this.state.intervaloDeAtualizacao)
+    }
+
+    render(){
         const conta = (
             <div className={styles.conta}>
-                <div className={styles.valor}>7</div>
-                <div className={styles.valor}>x</div>
-                <div className={styles.valor}>8</div>
+                {
+                    [this.state.n1, this.state.simbolo, this.state.n2, '=', '...'].map( (e, i) => (
+                        <div
+                            key={i.toString()} 
+                            className={styles.valor}>
+                            {e}
+                        </div>
+                    ))
+                }
             </div>
         )
-        const botoes = (
-            <div className={styles.botoes}>
-                <Button 
-                    label="Iniciar Jogo"
-                    className="p-button-raised"
-                    icon="pi pi-check"
-                />
-                <Button 
-                    label="Encerrar jogo"
-                    className="p-button-raised p-button-danger"
-                    icon="pi pi-times"
-                />
+
+        const alternativas = (
+            <div className={styles.alternativas}>
+                {
+                    this.state.alternativas.map ((alternativa, indice) => (
+                        <Button
+                            key={indice.toString()}
+                            label={alternativa?.toString()} 
+                            className={`${styles.valor} ${styles.alternativa}`}
+                        />
+                    ))
+                }
+            </div>
+        )
+
+        const tempoRestante = (
+            <div className={styles.tempoRestante}>
+                {this.state.tempoRestante}
             </div>
         )
         return (
-            <Card title={this.props.titulo} style={styles.card}>
-                <div className={styles.inner}>
-                    {conta}
-                    {botoes}
-                </div>
-            </Card>
+            <div>
+                {conta}
+                {alternativas}
+                {tempoRestante}
+            </div>
         )
     }
 }
 
 const styles = {
+    tempoRestante: 'flex justify-content-center align-items-center h-4rem text-3xl',
+    alternativa: 'p-button-outlined',
+    alternativas: 'flex justify-content-evenly align-items-center border-round shadow-2 h-4rem mt-2',
     conta: 'flex justify-content-center align-items-center border-round bg-orange-200 shadow-2 h-4rem',
-    valor: 'flex justify-content-center align-items-center border-round border-1 border-400 h-3rem w-3rem',
-    inner: 'border-round bg-orange-50 w-8 p-2 m-auto',
-    botoes: 'flex justify-content-evenly mt-5',
-    card: {
-        backgroundColor: 'var(--blue-100)'
-    }
+    valor: 'flex justify-content-center align-items-center border-round border-1 border-400 h-3rem w-3rem'
 }
 
-Jogo.defaultProps ={
-    titulo: 'Jogo de continhas'
-}
+// Jogo.defaultProps ={
+//     titulo: 'Jogo de continhas'
+// }
+
+// render() {
+//     const conta = (
+//         <div className={styles.conta}>
+//             <div className={styles.valor}>7</div>
+//             <div className={styles.valor}>x</div>
+//             <div className={styles.valor}>8</div>
+//         </div>
+//     )
+//     const botoes = (
+//         <div className={styles.botoes}>
+//             <Button 
+//                 label="Iniciar Jogo"
+//                 className="p-button-raised"
+//                 icon="pi pi-check"
+//             />
+//             <Button 
+//                 label="Encerrar jogo"
+//                 className="p-button-raised p-button-danger"
+//                 icon="pi pi-times"
+//             />
+//         </div>
+//     )
+//     return (
+//         <Card title={this.props.titulo} style={styles.card}>
+//             <div className={styles.inner}>
+//                 {conta}
+//                 {botoes}
+//             </div>
+//         </Card>
+//     )
+// }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,80 @@ import 'primeflex/primeflex.css'
 
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Cartao from './Cartao'
+import Mensagem from './Mensagem'
+import Botoes from './Botoes'
 import Jogo from './Jogo'
+// import Jogo from './Jogo'
 
 export default class App extends Component {
+
+    state = {
+        status: 'off',
+        acertos: 0,
+        erros: 0,
+        contador: 0
+    }
+
+    alterarStatus = (status) => {
+        this.setState({status})
+    }
+
+    atualizarPontuacao = (acertou) => {
+        this.setState(
+            acertou ? 
+            {acertos: this.state.acertos + 1} : 
+            {erros: this.state.erros + 1}
+        )
+        this.setState({contador: this.state.contador + 1})
+    }
+
+    zerarPontuacao = () => {
+        this.setState({
+            acertos: 0,
+            erros: 0,
+            contador: 0
+        })
+    }
+
     render() {
         return (
-            <Jogo />
+            <div className="grid justify-content-center">
+                <div className="col-12 lg:col-6">
+                    <Cartao className="h-18rem">
+                        <div className="h-12rem">
+                            {
+                                this.state.status === 'on' ?
+                                <Jogo 
+                                    status={this.state.status}
+                                    fAtualizarPontuacao={this.atualizarPontuacao}
+                                />
+                                :
+                                <div className="flex align-items-center justify-content-center h-full">
+                                    <Mensagem 
+                                        texto="Clique para iniciar"
+                                        className="w-6"
+                                    />
+                                </div>
+                            }
+
+                        </div>
+                        <Botoes
+                            fIniciar={() => this.alterarStatus('on')}
+                            fEncerrar={() => this.alterarStatus('off')}
+                            fZerar={() => this.zerarPontuacao()} 
+                            className="mt-5"
+                        />
+                    </Cartao>
+                </div>
+                <div className="col-12 lg:col-6">
+                    <Cartao
+                        titulo="Sua pontuaçao"
+                        className="h-18rem">
+
+                    </Cartao>
+                </div>
+            </div>
         )
     }
 }
